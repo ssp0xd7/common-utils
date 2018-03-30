6da9d62a1e75d0dcb8a097927c087cb0d130bac0
kevin <ssp0xd7@gmail.com>
2018-03-30 15:57:12 +0800

    add:添加LRU的简单实现

---
+++ src/main/java/com/common/utils/cache/LRUCache.java
@@ -0,0 +1,64 @@
+package com.common.utils.cache;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+/**
+ * LRU 缓存简单实现，基于HashMap
+ * 
+ * @author kevin(ssp0xd7 @ gmail.com) 30/03/2018
+ */
+public class LRUCache<K, V> {
+
+    /**
+     * 默认加载因子
+     */
+    private static final float factor = 0.75F;
+
+    private Map<K, V> map;
+
+    private int cacheSize;
+
+    public LRUCache(int cacheSize) {
+        this.cacheSize = cacheSize;
+        int capacity = (int) Math.ceil((double) ((float) cacheSize / factor)) + 1;
+        this.map = new LinkedHashMap<K, V>(capacity, 0.75F, true) {
+            private static final long serialVersionUID = -6265505777659103537L;
+
+            protected boolean removeEldestEntry(Map.Entry<K, V> eldest) {
+                return this.size() > LRUCache.this.cacheSize;
+            }
+        };
+    }
+
+    public synchronized V get(K key) {
+        return this.map.get(key);
+    }
+
+    public synchronized void put(K key, V value) {
+        this.map.put(key, value);
+    }
+
+    public synchronized void remove(K key) {
+        this.map.remove(key);
+    }
+
+    public synchronized void clear() {
+        this.map.clear();
+    }
+
+    public synchronized int usedEntries() {
+        return this.map.size();
+    }
+
+    @SuppressWarnings("unchecked")
+    public synchronized Collection<Map.Entry<K, V>> getAll() {
+        return new ArrayList(this.map.entrySet());
+    }
+
+    public interface CacheResizeListener<K, V> {
+        void onEntryToDel(Map.Entry<K, V> var1);
+    }
+}
--- src/main/java/com/common/utils/list/ListUtil.java
+++ src/main/java/com/common/utils/list/ListUtil.java
@@ -1,13 +1,12 @@
 package com.common.utils.list;
 
-import java.lang.reflect.Method;
+import java.lang.reflect.Field;
 import java.lang.reflect.Type;
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
-import org.apache.commons.lang3.StringUtils;
 
 /**
  * 对象的排序,排序字段必须包含get方法
@@ -19,24 +18,24 @@ public class ListUtil<T> {
 
     /**
      * 判断list是否为空列表
-     * 
+     *
      * @param list
      * @return
      */
     public static <T> boolean isEmptyList(List<T> list) {
-        return list == null || list.size() == 0;
+        return list == null || list.isEmpty();
     }
 
     /**
      * XXX 此方法只区分了int long float double boolean，其他参数类型都直接使用toString之后再比较
-     * 
+     *
      * @param list
      * @param field
      * @param sortDirection
      */
     public static <T> void sort(List<T> list, final String field, final SortDirection sortDirection) {
         list = trimToEmpty(list);
-        Collections.sort(list, new Comparator<T>() {
+        list.sort(new Comparator<T>() {
             private int typeCompare(Type returnType, Object o1, Object o2) {
                 if (Integer.TYPE.equals(returnType) || Integer.class.equals(returnType)) {
                     return ((Integer) o1).compareTo((Integer) o2);
@@ -57,33 +56,27 @@ public class ListUtil<T> {
 
             public int compare(T o1, T o2) {
                 int ret = 0;
-                String getMethod = parGetName(field);
-                Class[] params = new Class[0];
-                Object[] param = new Object[0];
-                try {
-                    Method m1 = o1.getClass().getMethod(getMethod, params);
-                    m1.setAccessible(true);
-                    Method m2 = o2.getClass().getMethod(getMethod, params);
-                    m2.setAccessible(true);
-                    Type returnType = m1.getReturnType();
 
-                    Object m1Result = m1.invoke(o1, param);
-                    Object m2Result = m1.invoke(o2, param);
+                try {
+                    Object field1 = getField(o1, field);
+                    Object field2 = getField(o2, field);
+
+                    Class<?> type = getType(o1, field);
 
                     if (sortDirection == SortDirection.DESC) {
-                        Object tmp = m1Result;
-                        m1Result = m2Result;
-                        m2Result = tmp;
+                        Object tmp = field1;
+                        field1 = field2;
+                        field2 = tmp;
                     }
 
-                    if (m1Result == null && m2Result == null) {
+                    if (field1 == null && field2 == null) {
                         return 0;
-                    } else if (m1Result == null) {
+                    } else if (field1 == null) {
                         return -1;
-                    } else if (m2Result == null) {
+                    } else if (field2 == null) {
                         return 1;
                     }
-                    ret = typeCompare(returnType, m1Result, m2Result);
+                    ret = typeCompare(type, field1, field2);
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
@@ -94,31 +87,35 @@ public class ListUtil<T> {
 
     /**
      * 对于List为null时返回空列表
-     * 
+     *
      * @param listMayNull
      * @return
      */
     public static <T> List<T> trimToEmpty(List<T> listMayNull) {
         if (listMayNull == null) {
-            return new ArrayList<T>();
+            return Collections.emptyList();
         } else {
             return listMayNull;
         }
     }
 
-    private static String parGetName(String fieldName) {
-        if (StringUtils.isBlank(fieldName)) {
-            return StringUtils.EMPTY;
-        }
+    private static Object getField(Object o, String fieldName) throws NoSuchFieldException, IllegalAccessException {
+        Field item = o.getClass().getDeclaredField(fieldName);
+        item.setAccessible(true);
+        return item.get(o);
+    }
 
-        return "get" + fieldName.substring(0, 1).toUpperCase() + fieldName.substring(1);
+    private static Class<?> getType(Object o, String fieldName) throws NoSuchFieldException {
+        Field item = o.getClass().getDeclaredField(fieldName);
+        item.setAccessible(true);
+        return item.getType();
     }
 
     public static <T> List<T> buildList(T... data) {
         if (data == null) {
-            return null;
+            return Collections.emptyList();
         }
-        ArrayList<T> list = new ArrayList<T>();
+        ArrayList<T> list = new ArrayList<>();
         Collections.addAll(list, data);
         return list;
     }
